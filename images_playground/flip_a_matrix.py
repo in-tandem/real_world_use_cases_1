a60f4d5c42c7d7f251917eb90e17730cffc3d807
somak <somaktukai@gmail.com>
2018-12-10 22:30:29 +0530

    added code for flipping matrix

--- images_playground/flip_a_matrix.py
+++ images_playground/flip_a_matrix.py
@@ -1,5 +1,5 @@
 import numpy as np 
-
+from compare_two_matrices import compare
 m = np.array([
 
     [5, 2, 3],
@@ -14,3 +14,21 @@ m = np.array([
 m_horizontally = m[: ,::-1]
 
 print(m_horizontally)
+
+assert compare(m_horizontally, np.fliplr(m))
+
+random_m = np.random.rand(4,5)
+
+assert compare(random_m[:,::-1], np.fliplr(random_m))
+
+## flip matrix vertically
+
+m_vertically = m[::-1]
+print(m_vertically)
+assert compare(random_m[::-1], np.flipud(random_m))
+assert compare(m_vertically, np.flipud(m))
+
+
+## lets combine both to get the matrix flipped horizontally and vertically as well
+
+assert compare( random_m[:,::-1][::-1], np.flipud(np.fliplr(random_m)))
